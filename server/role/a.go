a9643ecca298fa5972dba4f673ff867a96102344
coseman <904497924@qq.com>
2022-03-07 22:03:02 +0800

    维护角色模块统一入口

--- server/role/a.go
+++ server/role/a.go
@@ -6,8 +6,12 @@
 package role
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang-module/carbon/v2"
+	"github.com/goworkeryyt/aramis/server/role/api/v1"
 	"github.com/goworkeryyt/aramis/server/role/model"
 	"github.com/goworkeryyt/go-core/global"
+	"github.com/goworkeryyt/go-middle/record/operate"
 	"go.uber.org/zap"
 )
 
@@ -24,4 +28,40 @@ func autoCreateTables() {
 			global.LOG.Error("初始化表时异常：", zap.Any("err", err))
 		}
 	}
+}
+
+// RouterRegister 注册角色模块路由
+func RouterRegister(rGroup *gin.RouterGroup) {
+	autoCreateTables()
+	api := roleapi.ApiGroupApp
+	// 角色相关接口
+	roleGroup := rGroup.Group("role").Use(operate.OperateRecordHandler(carbon.DaysPerNormalYear))
+	{
+		// 新建角色
+		roleGroup.POST("createRole", api.CreateRole)
+		// 删除角色
+		roleGroup.POST("deleteRole", api.DeleteRole)
+		// 更新角色
+		roleGroup.POST("updateRole", api.UpdateRole)
+		// 角色菜单绑定
+		roleGroup.POST("roleMenuBind", api.RoleMenuBind)
+	}
+
+	// 无操作记录的接口
+	roleGroupWithoutRecord := rGroup.Group("role")
+	{
+		// 查询所有角色
+		roleGroupWithoutRecord.GET("findAllRoles", api.FindAllRoles)
+		// 查询单个角色
+		roleGroupWithoutRecord.GET("findRole", api.FindRole)
+		// 分页查询角色
+		roleGroupWithoutRecord.GET("findRolePage", api.GetRolePage)
+		// 获取角色菜单
+		roleGroupWithoutRecord.GET("getRoleMenu", api.GetRoleMenu)
+		// 获取自身菜单
+		roleGroupWithoutRecord.GET("getSelfMenuTree", api.GetSelfMenuTree)
+		// 获取自身按钮权限
+		roleGroupWithoutRecord.GET("getSelfButtons", api.GetSelfButtons)
+	}
+
 }
--- server/role/api/v1/a.go
+++ server/role/api/v1/a.go
@@ -6,12 +6,14 @@
 package roleapi
 
 import (
-	menusrv "github.com/goworkeryyt/aramis/server/menu/service"
+	"github.com/goworkeryyt/aramis/server/menu/service"
 	"github.com/goworkeryyt/aramis/server/role/service"
 )
 
 type ApiGroup struct {
 	RoleApi
+	RoleMenuApi
+	RoleButtonApi
 }
 
 var ApiGroupApp = new(ApiGroup)
@@ -19,5 +21,4 @@ var ApiGroupApp = new(ApiGroup)
 var roleService = rolesrv.ServiceGroupApp.RoleService
 var roleMenuService = rolesrv.ServiceGroupApp.RoleMenuService
 var roleButtonService = rolesrv.ServiceGroupApp.RoleButtonService
-
 var menuService = menusrv.ServiceGroupApp.MenuService
